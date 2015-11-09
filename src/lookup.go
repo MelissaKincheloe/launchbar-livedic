466ccdc655f470d5c5beeb03471e0f85556ab0fc
BlahGeek <i@BlahGeek.com>
2015-11-09 14:13:32 +0800

    fix unicode lookup

--- src/lookup.go
+++ src/lookup.go
@@ -10,8 +10,17 @@ const char*
 define(const char *s, int *start, int *len) {
   NSString *word = [[NSString alloc] initWithCString:s encoding:NSUTF8StringEncoding];
   CFRange termRange = DCSGetTermRangeInString(NULL, (CFStringRef)word, 0);
+
   *start = termRange.location;
   *len = termRange.length;
+
+  if(*start != -1) {
+    NSString *first_part = [word substringToIndex: *start];
+    NSString *second_part = [word substringWithRange: NSMakeRange(*start, *len)];
+    *start = [first_part lengthOfBytesUsingEncoding: NSUTF8StringEncoding];
+    *len = [second_part lengthOfBytesUsingEncoding: NSUTF8StringEncoding];
+  }
+
   NSString *definition = (NSString*)DCSCopyTextDefinition(NULL, (CFStringRef)word, termRange);
   return [definition UTF8String];
 }
